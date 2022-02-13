699e65920ac5b184bfe09f08e8f74062e93f7b1e
honggoii <honggoii.naver.com>
2022-02-13 19:22:07 +0900

    api 호출 테스트를 위한 임시 메인화면 추가

--- app/src/main/java/org/honggoii/bookcheck/activity/MainActivity.kt
+++ app/src/main/java/org/honggoii/bookcheck/activity/MainActivity.kt
@@ -6,31 +6,47 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.SearchView
-import org.honggoii.bookcheck.MyViewPagerAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import org.honggoii.bookcheck.R
 import org.honggoii.bookcheck.databinding.ActivityMainBinding
+import org.honggoii.bookcheck.viewmodel.BookViewModel
 
 class MainActivity : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
 
+    private lateinit var myViewModel: BookViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        myViewModel = ViewModelProvider(this).get(BookViewModel::class.java)
 
-        /* View Binding */
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            myViewModel.getBookSearch()
+        }
 
-        /* Toolbar */
-        setSupportActionBar(binding.toolbar)
+        myViewModel.book.observe(this, {
+//            binding.textView.text = it.category
+        })
+//        /* View Binding */
+//        val binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
 
-        /* ActionBarDrawerToggle 버튼 적용 */
-        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        toggle.syncState()
+//
+//        /* Toolbar */
+//        setSupportActionBar(binding.toolbar)
+//
+//        /* ActionBarDrawerToggle 버튼 적용 */
+//        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        toggle.syncState()
+//
+//        /* 뷰 페이저 어댑터 */
+//        val adapter = MyViewPagerAdapter(this)
+//        binding.viewpager.adapter = adapter
 
-        /* 뷰 페이저 어댑터 */
-        val adapter = MyViewPagerAdapter(this)
-        binding.viewpager.adapter = adapter
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
