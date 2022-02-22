6c79081247f8df4daeb5e6765fa09085aab2a4b7
honggoii <honggoii@naver.com>
2022-02-23 00:18:35 +0900

    기존 RecyclerView 제거

--- app/src/main/java/org/honggoii/bookcheck/MyViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package org.honggoii.bookcheck
-
-import androidx.recyclerview.widget.RecyclerView
-import org.honggoii.bookcheck.databinding.ItemListBinding
-
-class MyViewHolder(val binding: ItemListBinding): RecyclerView.ViewHolder(binding.root) {
-}
