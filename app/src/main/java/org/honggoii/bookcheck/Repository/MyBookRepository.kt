607862fe76999e8435ffb41e7d5d86a42ebeb35e
honggoii <honggoii@naver.com>
2022-05-22 22:06:52 +0900

    책 저장 메서드 임시 추가

--- app/src/main/java/org/honggoii/bookcheck/Repository/MyBookRepository.kt
+++ app/src/main/java/org/honggoii/bookcheck/Repository/MyBookRepository.kt
@@ -2,6 +2,9 @@ package org.honggoii.bookcheck.Repository
 
 import android.app.Application
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.honggoii.bookcheck.Database.MyBookDatabase
 import org.honggoii.bookcheck.dao.MyBookDao
 import org.honggoii.bookcheck.entity.MyBook
@@ -9,11 +12,21 @@ import org.honggoii.bookcheck.entity.MyBook
 class MyBookRepository(application: Application) {
     val myBookListResults = MutableLiveData<List<MyBook>>()
 
-    
-
     private var myBookDao: MyBookDao?
     init {
         val db: MyBookDatabase? = MyBookDatabase.getInstance(application)
         myBookDao = db?.myBookDao() // 데이터베이스 인스턴스를 통해 dao 참조
+    }
+
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    fun insertMyBook(newBook: MyBook) {
+        coroutineScope.launch(Dispatchers.IO) {
+            asyncInsert(newBook)
+        }
+    }
+
+    private suspend fun asyncInsert(myBook: MyBook) {
+        myBookDao?.insert(myBook)
     }
 }
